7132b62fd9ee30c789a38d6175d6e76a1f226e1f
Chloe Court <crc.courtois@gmail.com>
2023-03-14 21:23:31 +0100

    added api call to foursquare

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,33 @@ import Card from "@/components/Card/Card";
 import mockData from "../data/mockData.json";
 
 export async function getStaticProps(context: any) {
-  return {
-    props: {
-      mockData,
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "fsq3boRbY72zaaGzGWe2QslhgNW7Xiyoq/KNJk/0BJ3uod0=",
     },
   };
+  try {
+    const response = await fetch(
+      "https://api.foursquare.com/v3/places/search?query=restaurants&near=Madrid%2C%20Spain&sort=RELEVANCE&limit=10",
+      options
+    );
+    const fourSquareData = await response.json();
+    const { results } = fourSquareData;
+    if (!results) {
+      throw console.error();
+    }
+    return {
+      props: {
+        restaurants: results,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
-export default function Home({ mockData }: any) {
+export default function Home({ restaurants }: any) {
   return (
     <>
       <Head>
@@ -30,18 +50,18 @@ export default function Home({ mockData }: any) {
             console.log("clicked");
           }}
         />
-        {mockData.length && (
+        {restaurants.length && (
           <h2 className={styles.locationHeading}>Justicia</h2>
         )}
         <section className={styles.cardLayout}>
-          {mockData.map((resto: any) => {
-            const { name, imgUrl, id, websiteUrl: href } = resto;
+          {restaurants.map((resto: any) => {
+            const { name, imgUrl, fsq_id: id, websiteUrl: href } = resto;
             return (
               <Card
                 key={id}
                 name={name}
-                imgUrl={imgUrl}
-                href={`restaurant/${id}`}
+                imgUrl={imgUrl || "/static/restaurantDummyImage.jpg"}
+                // href={`restaurant/${id}`}
                 alt={`${name} restaurant`}
               />
             );
