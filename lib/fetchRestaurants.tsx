b3037bf64d5b47ecf46f9c085a1a7dd400e4a5bd
Chloe Court <crc.courtois@gmail.com>
2023-03-17 21:10:57 +0100

    added api call to unsplash api

--- lib/fetchRestaurants.tsx
+++ lib/fetchRestaurants.tsx
@@ -1,3 +1,24 @@
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+  accessKey: `${process.env.UNSPLASH_ACCESS_KEY}`,
+});
+
+const getImgUrls = async () => {
+  try {
+    const photos = await unsplash.search.getPhotos({
+      query: "restaurant",
+      page: 1,
+      perPage: 10,
+    });
+
+    const unsplashedResults = photos?.response?.results;
+    return unsplashedResults!.map((data: any) => data.urls["small"]);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 type getFourSquareUrlObject = {
   // latLong?: string;
   limit: number;
@@ -17,15 +38,8 @@ export const getFourSquareUrl = ({
   return `https://api.foursquare.com/v3/places/search?query=${query}&near=${city}%2C%20${country}&sort=${sort}&limit=${limit}`;
 };
 
-getFourSquareUrl({
-  limit: 10,
-  query: "restaurants",
-  city: "Madrid",
-  country: "Spain",
-  sort: "RELEVANCE",
-});
-
 export const fetchRestaurants = async () => {
+  const image = await getImgUrls();
   const options = {
     method: "GET",
     headers: {
@@ -35,7 +49,6 @@ export const fetchRestaurants = async () => {
   };
   try {
     const response = await fetch(
-      // "https://api.foursquare.com/v3/places/search?query=restaurants&near=Madrid%2C%20Spain&sort=RELEVANCE&limit=10",
       getFourSquareUrl({
         limit: 10,
         query: "restaurants",
@@ -51,7 +64,21 @@ export const fetchRestaurants = async () => {
     if (!results) {
       throw console.error();
     }
-    return results;
+    // destructured objects to create new object with specific key value pairs needed
+    return results.map(
+      (
+        { location: { formatted_address, locality }, name, fsq_id }: any,
+        index: number
+      ) => {
+        return {
+          address: formatted_address,
+          neighborhood: locality,
+          name,
+          id: fsq_id,
+          imgUrl: image!.length > 0 ? image![index] : null,
+        };
+      }
+    );
   } catch (e) {
     console.error(e);
   }
