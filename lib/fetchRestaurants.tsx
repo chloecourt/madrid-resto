7db7de5154208c4c61683f63e1ce6f7629c864a2
Chloe Court <crc.courtois@gmail.com>
2023-05-03 21:55:07 +0200

    changed query for api still not working

--- lib/fetchRestaurants.tsx
+++ lib/fetchRestaurants.tsx
@@ -4,41 +4,39 @@ const unsplash = createApi({
   accessKey: `${process.env.UNSPLASH_ACCESS_KEY}`,
 });
 
+type GetUrlForRestaurantsParams = {
+  latLong: string;
+  query: string;
+  limit: number;
+};
+const getUrlForRestaurants = ({
+  latLong,
+  query,
+  limit,
+}: GetUrlForRestaurantsParams) => {
+  return `https://api.foursquare.com/v3/places/search?query=${query}&ll=${latLong}&limit=${limit}`;
+};
 const getImgUrls = async () => {
   try {
     const photos = await unsplash.search.getPhotos({
       query: "restaurant",
-      page: 1,
-      perPage: 10,
+      perPage: 30,
     });
-
+    console.log({ photos });
     const unsplashedResults = photos?.response?.results;
+    if (!unsplashedResults) {
+      throw Error("photo api images did not load");
+    }
     return unsplashedResults!.map((data: any) => data.urls["small"]);
   } catch (e) {
     console.error(e);
   }
 };
 
-type getFourSquareUrlObject = {
-  // latLong?: string;
-  limit: number;
-  city?: string;
-  country?: string;
-  sort: string;
-  query: string;
-};
-export const getFourSquareUrl = ({
-  // latLong,
-  limit,
-  query,
-  city,
-  country,
-  sort = "RELEVANCE",
-}: getFourSquareUrlObject) => {
-  return `https://api.foursquare.com/v3/places/search?query=${query}&near=${city}%2C%20${country}&sort=${sort}&limit=${limit}`;
-};
-
-export const fetchRestaurants = async () => {
+export const fetchRestaurants = async (
+  latLong = "40.422881%2C-3.685774",
+  limit = 6
+) => {
   const image = await getImgUrls();
   const options = {
     method: "GET",
@@ -47,39 +45,30 @@ export const fetchRestaurants = async () => {
       Authorization: process.env.FOURSQUARE_API_KEY!,
     },
   };
-  try {
-    const response = await fetch(
-      getFourSquareUrl({
-        limit: 10,
-        query: "restaurants",
-        city: "Madrid",
-        country: "Spain",
-        sort: "RELEVANCE",
-      }),
-      options
-    );
-    const fourSquareData = await response.json();
-    const { results } = fourSquareData;
 
-    if (!results) {
-      throw console.error();
-    }
-    // destructured objects to create new object with specific key value pairs needed
-    return results.map(
-      (
-        { location: { formatted_address, locality }, name, fsq_id }: any,
-        index: number
-      ) => {
-        return {
-          address: formatted_address,
-          neighborhood: locality,
-          name,
-          id: fsq_id,
-          imgUrl: image!.length > 0 ? image![index] : null,
-        };
-      }
-    );
-  } catch (e) {
-    console.error(e);
+  const response = await fetch(
+    getUrlForRestaurants({ latLong, query: "restaurants", limit: 6 }),
+    options
+  );
+  const fourSquareData = await response.json();
+  const { results } = fourSquareData;
+
+  if (!results) {
+    throw console.error();
   }
+  // destructured objects to create new object with specific key value pairs needed
+  return results.map(
+    (
+      { location: { formatted_address, locality }, name, fsq_id }: any,
+      index: number
+    ) => {
+      return {
+        address: formatted_address,
+        neighborhood: locality,
+        name,
+        id: fsq_id,
+        imgUrl: image!.length > 0 ? image![index] : null,
+      };
+    }
+  );
 };
