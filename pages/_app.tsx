125a8b679cd14ec1b92e0f32f4706d4773754785
Chloe Court <crc.courtois@gmail.com>
2023-05-26 17:14:00 +0200

    added boilerplate for react context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,40 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Nunito } from "@next/font/google";
+import { createContext, useState } from "react";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
+type InitialStateType = {
+  latLong: "";
+  restaurants: [];
+};
+
+type AppContextData = {
+  state: InitialStateType;
+};
+
+const AppContext = createContext<AppContextData | null>(null);
+
+const AppProvider = ({ children }: any) => {
+  const initialState: InitialStateType = {
+    latLong: "",
+    restaurants: [],
+  };
+
+  return (
+    <AppContext.Provider value={{ state: initialState }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={nunito.className}>
-      <Component {...pageProps} />
-    </div>
+    <AppProvider>
+      <div className={nunito.className}>
+        <Component {...pageProps} />
+      </div>
+    </AppProvider>
   );
 }
