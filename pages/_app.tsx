8839c3564027250d673a1f2aff58aa59e6976289
Chloe Court <crc.courtois@gmail.com>
2023-06-08 14:03:58 +0200

    added useReducer into Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,65 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Nunito } from "@next/font/google";
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
+type RestaurantObjectType = {
+  address: string;
+  neighborhood: string;
+  name: string;
+  id: string;
+  imgUrl: string;
+};
+
 type InitialStateType = {
-  latLong: "";
-  restaurants: [];
+  latLong: string;
+  restaurants: RestaurantObjectType[];
 };
 
-type AppContextData = {
+const initialState: InitialStateType = {
+  latLong: "",
+  restaurants: [],
+};
+
+export const ACTION_TYPES = {
+  SET_LAT_LONG: "SET_LAT_LONG",
+  SET_RESTAURANTS: "SET_RESTAURANTS",
+};
+
+export type AppContextType = {
   state: InitialStateType;
+  dispatch: React.Dispatch<LatLongAction>;
+};
+interface LatLongAction {
+  type: "SET_LAT_LONG" | "SET_RESTAURANTS";
+  payload: InitialStateType;
+}
+
+const appReducer = (state: InitialStateType, action: LatLongAction) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return {
+        ...state,
+        latLong: action.payload.latLong,
+      };
+    }
+    case ACTION_TYPES.SET_RESTAURANTS: {
+      return { ...state, restaurants: action.payload.restaurants };
+    }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
 };
 
-const AppContext = createContext<AppContextData | null>(null);
+export const AppContext = createContext<AppContextType | null>(null);
 
-const AppProvider = ({ children }: any) => {
-  const initialState: InitialStateType = {
-    latLong: "",
-    restaurants: [],
-  };
+const AppProvider = ({ children }: { children: JSX.Element }) => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
 
   return (
-    <AppContext.Provider value={{ state: initialState }}>
+    <AppContext.Provider value={{ state, dispatch }}>
       {children}
     </AppContext.Provider>
   );
